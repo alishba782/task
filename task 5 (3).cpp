7c3ac1ea6633fa6df45633b766fb4acd9f3b6d22
alishba782 <alishbah602@gmail.com>
2024-10-24 17:48:37 +0500

    Add files via upload

---
+++ task 5 (3).cpp
@@ -0,0 +1,20 @@
+#include<isostream>
+using namespace std;
+void main()
+{
+	for (int i = 0; i < size; i++)
+	{
+		for (intj = 0; j < size - i; j++)
+			count << "*";
+	}
+	cout << "*";
+	if (i>0)
+		(
+	for (int j = 0; j < 2 * i - 1; j++)
+		cout << "*";
+}
+cout << endl;
+}
+system("pause");
+return 0;
+}
